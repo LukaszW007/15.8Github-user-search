2af2c6a5a9cd14e23876ace6d110ec2d464cd445
LukaszWiszniewski <wisznu07@gmail.com>
2018-02-22 21:23:43 +0100

    Added user,userslist classes

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-ReactDOM.render(
-<App />,
-    document.getElementById('root')
-);
+
 
 class App extends React.Component {
     constructor() {
@@ -41,3 +38,32 @@ class App extends React.Component {
         );
     }
 }
+
+class UsersList extends React.Component {
+    get users() {
+        return this.props.users.map(user => <User key={user.id} user={user}/>);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.users}
+            </div>
+        );
+    }
+}
+
+class User extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+                <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
